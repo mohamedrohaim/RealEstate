9f270797749391000ec4cb21245e7348897decfb
mohamedRohiem <mohamedrohiem597@gmail.com>
2024-02-05 12:51:12 +0200

    initial commit

---
+++ DataAccessLayer/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+	public class AppDbContext : DbContext{
+		public AppDbContext(DbContextOptions options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+
+
+
+	}
+}
---
+++ SampleApi/Program.cs
@@ -0,0 +1,45 @@
+using DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+
+namespace SampleApi
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddDbContext<AppDbContext>(options =>
+			options.UseSqlServer(
+				builder.Configuration.GetConnectionString("DefaultConnection")
+				));
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+			builder.Services.AddCors();
+
+			var app = builder.Build();
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseCors(option=>option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
